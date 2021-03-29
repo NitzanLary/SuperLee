da44346e4ca2cf5e081dccdaa19645f565809e0f
RefaelFarjun <farjunr@post.bgu.ac.il>
2021-03-29 17:27:13 +0300

    Add data types to some classes

--- dev/Delivery/Task.java
+++ dev/Delivery/Task.java
@@ -1,4 +1,16 @@
 package Delivery;
 
+import java.util.HashMap;
+
 public class Task {
+    private String id;
+    private HashMap<String, Integer>listOfproduct;
+    private String loadingOrunloading;
+
+    public Task(String id, HashMap<String, Integer> listOfproduct, String loadingOrunloading){
+        this.id = id;
+        this.listOfproduct = listOfproduct;
+
+
+    }
 }
--- dev/Delivery/Truck.java
+++ dev/Delivery/Truck.java
@@ -5,4 +5,12 @@ public class Truck {
     private String model;
     private int maxWeight;
     private int truckWeight;
+
+    public Truck(String id, String model, int maxWeight, int truckWeight){
+        this.id = id;
+        this.model = model;
+        this.maxWeight = maxWeight;
+        this.truckWeight = maxWeight;
+    }
+
 }
