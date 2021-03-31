4998162a451485523421f896320bd1fba80f30fb
Yanay <yanayshem@gmail.com>
2021-03-31 21:46:36 +0300

    test git

--- dev/Main.java
+++ dev/Main.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        //System.out.println("!Hello world");
-        System.out.println("this is some test");
-        int x = 3;
+        System.out.println("!Hello world");
     }
 }
