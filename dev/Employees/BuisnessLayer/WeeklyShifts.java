2a3249d9b0d304c6f291a8a102b9540e27cf6172
Yanay <yanayshem@gmail.com>
2021-03-31 22:09:47 +0300

    added WeeklyShifts Class with getters and setters.

---
+++ dev/Employees/BuisnessLayer/WeeklyShifts.java
@@ -0,0 +1,29 @@
+package Employees.BuisnessLayer;
+
+import java.util.Date;
+
+public class WeeklyShifts {
+
+    private Date fromDate;
+    private Date toDate;
+
+
+
+    public Date getFromDate() {
+        return fromDate;
+    }
+
+    public void setFromDate(Date fromDate) {
+        this.fromDate = fromDate;
+    }
+
+    public Date getToDate() {
+        return toDate;
+    }
+
+    public void setToDate(Date toDate) {
+        this.toDate = toDate;
+    }
+
+
+}
