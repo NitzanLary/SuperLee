9cf4f12a85b5cb963590d48fed0b5642fe6d7e97
Asaf Stern <asafste@post.bgu.ac.il>
2021-03-31 13:30:52 +0300

    Facade controller built

---
+++ dev/Delivery/FacadeController.java
@@ -0,0 +1,45 @@
+package Delivery;
+
+import javax.sound.midi.Soundbank;
+import java.util.Scanner;
+
+public class FacadeController {
+    DeliveryController dec;
+    DriverController drc;
+    AreaController arc;
+    TaskController tac;
+    TruckController trc;
+
+    public FacadeController(){
+        dec = new DeliveryController();
+        drc = new DriverController();
+        arc = new AreaController();
+        tac = new TaskController();
+        trc = new TruckController();
+    }
+
+    public void runWithConsole(){
+        Scanner in = new Scanner(System.in);
+        System.out.println("welcome to Delivery Module!");
+        System.out.println("for exit the simulation type 'exit' as input at any point");
+        String s = "";
+        while (!s.equals("exit")){
+            System.out.println("system current state:");
+            System.out.println(toString());
+            System.out.println("chose action:\n1 add new delivery,\n2 update existing delivery\n3 create new appending task" +
+                    "\n4 add Truck to the sys\n5 add Driver to the sys\nadd Area to the sys\n7 add location to the sys");
+            s = in.nextLine();
+
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "dec=" + dec +
+                ", drc=" + drc +
+                ", arc=" + arc +
+                ", tac=" + tac +
+                ", trc=" + trc +
+                '}';
+    }
+}
