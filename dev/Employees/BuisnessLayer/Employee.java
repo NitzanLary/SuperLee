9fcc90b06703ad3697976a8a5942a4997112b793
Nitzan <laryn@post.bgu.ac.il>
2021-03-31 22:25:42 +0300

    added fields to Employee.

--- dev/Employees/BuisnessLayer/Employee.java
+++ dev/Employees/BuisnessLayer/Employee.java
@@ -1,12 +1,21 @@
 package Employees.BuisnessLayer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Employee {
     private String name;
     private String ID;
     private String bankAccount;
     private int salary;
+    private List<Role> roles;
+    private TermsOfEmployee terms;
 
-    Employee(String _name, String _ID) {name = _name; ID = _ID;}
+    Employee(String _name, String _ID) {
+        name = _name;
+        ID = _ID;
+        roles = new ArrayList<Role>();
+    }
 
     public String getName() {return name;}
 
@@ -22,6 +31,14 @@ public class Employee {
         return ID;
     }
 
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+    public TermsOfEmployee getTerms() {
+        return terms;
+    }
+
     public void setSalary(int salary) {
         this.salary = salary;
     }
@@ -30,5 +47,11 @@ public class Employee {
         this.bankAccount = bankAccount;
     }
 
+    public void AddRole(Role role){
+        roles.add(role);
+    }
 
+    public void setTerms(TermsOfEmployee terms) {
+        this.terms = terms;
+    }
 }
