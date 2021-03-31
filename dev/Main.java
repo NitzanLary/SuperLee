1646361a62151799cfa86d7734d897fc4cd5ff79
Nitzan <laryn@post.bgu.ac.il>
2021-03-31 21:29:00 +0300

    ive wrote a "this is a test" and assigned 3 to x.

--- dev/Main.java
+++ dev/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("!Hello world");
+        //System.out.println("!Hello world");
+        System.out.println("this is some test");
+        int x = 3;
     }
 }
