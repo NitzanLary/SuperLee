26f966bd7f824b6e58b3aa4bc3d3df42ea6ba6b8
Yanay <yanayshem@gmail.com>
2021-04-01 11:47:41 +0300

    .

--- dev/Main.java
+++ dev/Main.java
@@ -10,26 +10,21 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
 //        System.out.println("!Hello world");
-//        WeeklyShifts ws = new WeeklyShifts(LocalDate.now(), LocalDate.now().plusDays(7));
-//        for(int i = 0 ; i < ws.getShifts().size() ; i++){
-//            System.out.println(ws.getShifts().get(i).getDate());
-//            System.out.println(ws.getShifts().get(i).getStart());
-//            System.out.println(ws.getShifts().get(i).getEnd());
-//            System.out.println("-----------------------------------");
-//            LocalDateTime hour = LocalDate.now().atTime(14,30);
-        ShiftController sc = ShiftController.getInstance();
-        ShiftController.add4WeeksSlots();
-        for (int i = 0 ; i < sc.getWeeklyShifts().size() ; i++) {
-            System.out.println("\n");
-            System.out.println("  Week: " + sc.getWeeklyShifts().get(i).getFromDate() + " Until the " + sc.getWeeklyShifts().get(i).getToDate());
-            System.out.println("   " + "-----------------------------------");
 
-            for (int j = 0; j < sc.getWeeklyShifts().get(i).getShifts().size(); j++) {
-                System.out.println("|  " + sc.getWeeklyShifts().get(i).getShifts().get(j).getDate());
-                System.out.println("|  " + sc.getWeeklyShifts().get(i).getShifts().get(j).getStart());
-                System.out.println("|  " + sc.getWeeklyShifts().get(i).getShifts().get(j).getEnd());
-                System.out.println("  -----------------------------------");
-//            LocalDateTime hour = LocalDate.now().atTime(14,30);
+
+
+//        ShiftController sc = ShiftController.getInstance();
+//        ShiftController.add4WeeksSlots();
+//        for (int i = 0 ; i < sc.getWeeklyShifts().size() ; i++) {
+//            System.out.println("\n");
+//            System.out.println("  Week: " + sc.getWeeklyShifts().get(i).getFromDate() + " Until the " + sc.getWeeklyShifts().get(i).getToDate());
+//            System.out.println("   " + "-----------------------------------");
+//
+//            for (int j = 0; j < sc.getWeeklyShifts().get(i).getShifts().size(); j++) {
+//                System.out.println("|  " + sc.getWeeklyShifts().get(i).getShifts().get(j).getDate());
+//                System.out.println("|  " + sc.getWeeklyShifts().get(i).getShifts().get(j).getStart());
+//                System.out.println("|  " + sc.getWeeklyShifts().get(i).getShifts().get(j).getEnd());
+//                System.out.println("  -----------------------------------");
             }
 
         }
