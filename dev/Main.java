f73fb9f197a5be4546731fc64bac698f8365b237
Nitzan <laryn@post.bgu.ac.il>
2021-05-07 21:49:20 +0300

    adding DriverRole class

--- dev/Main.java
+++ dev/Main.java
@@ -5,11 +5,6 @@ import java.time.LocalTime;
 public class Main {
     public static void main(String[] args) {
 //        CLI.getInstance().start();
-        LocalTime time = LocalTime.of(12,0);
-        LocalTime start = LocalTime.of(12,0);
-        LocalTime end = LocalTime.of(14,0);
-        System.out.println(time.equals(start));
-//        System.out.println(time.isBefore(end));
     }
 
 }
