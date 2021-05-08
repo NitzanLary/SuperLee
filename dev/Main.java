f04744be255c76b1654d586922330066d927c579
RefaelFarjun <farjunr@post.bgu.ac.il>
2021-05-08 12:05:34 +0300

    integrate with Employee bt taking driver from Employee module

--- dev/Main.java
+++ dev/Main.java
@@ -1,15 +1,22 @@
-import Employees.PresentationLayer.CLI;
+import Delivery.PresentationLayer.CLI;
 
+import java.time.LocalDate;
 import java.time.LocalTime;
 
 public class Main {
     public static void main(String[] args) {
 //        CLI.getInstance().start();
+        CLI cli = new CLI();
         LocalTime time = LocalTime.of(12,0);
         LocalTime start = LocalTime.of(12,0);
         LocalTime end = LocalTime.of(14,0);
         System.out.println(time.equals(start));
-//        System.out.println(time.isBefore(end));
+        System.out.println(time.isBefore(end));
+
+        LocalDate date = cli.getFacade().parseToLocalDate("12-12-12");
+        LocalTime t = cli.getFacade().parseToLocalTime("12:12");
+        System.out.println(date);
+        System.out.println(t);
     }
 
 }
