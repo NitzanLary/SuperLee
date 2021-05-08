3307fba5fe4b74c80bf4da24f20a2737af376520
Aviehu <aviehu@gmail.com>
2021-05-08 12:35:43 +0300

    test

--- dev/Suppliers_Inv/src/DataLayer/DAO/DAO.java
+++ dev/Suppliers_Inv/src/DataLayer/DAO/DAO.java
@@ -7,7 +7,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public abstract class DAO {
-    private final String url = "jdbc:sqlite:dev/DataBase/SuperLeeDB.db";
+    private final String url = "jdbc:sqlite:dev/Suppliers_Inv/src/DataLayer/InventoryDB.db";
 
     protected ResponseT<Connection> getConn() {
         Connection conn = null;
